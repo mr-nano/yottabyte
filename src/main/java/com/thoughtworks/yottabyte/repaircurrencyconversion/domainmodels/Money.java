75ada0a94be38d3bb4c898e29e747c3e9bf89968
Dhruv Kapur <dhruvk@thoughtworks.com>
2015-05-23 16:52:17 +0530

    Add money.

---
+++ src/main/java/com/thoughtworks/yottabyte/repaircurrencyconversion/domainmodels/Money.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.yottabyte.repaircurrencyconversion.domainmodels;
+
+import com.thoughtworks.yottabyte.repaircurrencyconversion.domainmodels.Currency.Currency;
+import lombok.Getter;
+
+@Getter
+public class Money {
+  private final Currency currency;
+  private final Double amount;
+
+  public Money(Currency currency, Double amount) {
+    this.currency = currency;
+    this.amount = amount;
+  }
+
+  public Money convertTo(Currency to) {
+    if(eitherCurrenciesAreUnknown(currency,to)){
+      return this;
+    }
+    return new Money(to,amount*getCurrency().getConversionFactor(to));
+  }
+
+  private boolean eitherCurrenciesAreUnknown(Currency one, Currency two){
+    return one.isUnknown() || two.isUnknown();
+  }
+}
---
+++ src/test/java/com/thoughtworks/yottabyte/repaircurrencyconversion/domainmodels/MoneyTest.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.yottabyte.repaircurrencyconversion.domainmodels;
+
+import com.googlecode.zohhak.api.TestWith;
+import com.googlecode.zohhak.api.runners.ZohhakRunner;
+import com.thoughtworks.yottabyte.repaircurrencyconversion.domainmodels.Currency.Currency;
+import org.junit.runner.RunWith;
+
+import static com.thoughtworks.yottabyte.repaircurrencyconversion.domainmodels.Currency.Currency.UNKNOWN;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(ZohhakRunner.class)
+public class MoneyTest {
+
+  @TestWith({
+    "DOLLARS, 10, RUPEES, 634.90",
+    "POUNDS, 34, DOLLARS, 52.292",
+    "DOLLARS, 1, DOLLARS, 1",
+  })
+  public void shouldBeAbleToConvertFromAnother(Currency originalCurrency, Double originalAmount,
+                                               Currency newCurrency, Double newExpectedAmount){
+    Money originalMoney = new Money(originalCurrency,originalAmount);
+    Money newMoney = originalMoney.convertTo(newCurrency);
+    assertThat(newMoney.getCurrency(),is(newCurrency));
+    assertThat(newMoney.getAmount(),is(newExpectedAmount));
+  }
+
+  @TestWith({
+    "1, DOLLARS",
+    "10, UNKNOWN",
+  })
+  public void shouldNotConvertIfItsOriginallyInUnknownCurrency(Double originalAmount, Currency newCurrency){
+    Money originalMoney = new Money(UNKNOWN,originalAmount);
+    Money newMoney = originalMoney.convertTo(newCurrency);
+    assertThat(newMoney.getCurrency(),is(UNKNOWN));
+    assertThat(newMoney.getAmount(),is(originalAmount));
+  }
+
+}
