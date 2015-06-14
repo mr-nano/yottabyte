a0645e94370e4af243ad0e75b951aefeb8e56a01
Dhruv Kapur <dhruvk@thoughtworks.com>
2015-06-14 15:48:52 +0530

    Add absolute function to partitioner

--- src/main/java/com/thoughtworks/yottabyte/vehiclerepairdenormalization/secondarysortstrategy/Driver.java
+++ src/main/java/com/thoughtworks/yottabyte/vehiclerepairdenormalization/secondarysortstrategy/Driver.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.yottabyte.vehiclerepairdenormalization.secondarysortstrategy;
 
 import com.google.common.base.Preconditions;
-import com.thoughtworks.yottabyte.vehiclefiltercount.OldVehicleCountDriver;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
--- src/main/java/com/thoughtworks/yottabyte/vehiclerepairdenormalization/secondarysortstrategy/Partitioner.java
+++ src/main/java/com/thoughtworks/yottabyte/vehiclerepairdenormalization/secondarysortstrategy/Partitioner.java
@@ -2,9 +2,11 @@ package com.thoughtworks.yottabyte.vehiclerepairdenormalization.secondarysortstr
 
 import org.apache.hadoop.io.Text;
 
+import static java.lang.Math.abs;
+
 public class Partitioner extends org.apache.hadoop.mapreduce.Partitioner<TaggedKey,Text> {
   @Override
   public int getPartition(TaggedKey key, Text taggedText, int numberOfReducers) {
-    return key.getVehicleType().hashCode() % numberOfReducers;
+    return abs(key.getVehicleType().hashCode()) % numberOfReducers;
   }
 }
