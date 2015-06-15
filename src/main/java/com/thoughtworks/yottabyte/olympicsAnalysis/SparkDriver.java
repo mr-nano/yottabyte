18f315f631de787752bdffe52c97d267f603b174
Rahul Kavale <kavale.rahul@gmail.com>
2015-06-15 15:15:11 +0530

    Rahul Kavale - added data files for spark examples

---
+++ src/main/java/com/thoughtworks/yottabyte/olympicsAnalysis/SparkDriver.java
@@ -0,0 +1,52 @@
+package com.thoughtworks.yottabyte.olympicsAnalysis;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+public class SparkDriver {
+    public static void main(String[] args) {
+        String master = args[0];
+        String appName = args[1];
+        String path = args[2];
+        String destination = args[3];
+
+        SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+
+        JavaRDD<String> lines = sc.textFile(path);
+
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterable<String> call(String s) throws Exception {
+                return Arrays.asList(s.split(" "));
+            }
+        });
+
+        JavaPairRDD<String, Integer> stringIntegerJavaPairRDD = words.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                return new Tuple2<>(s, 1);
+            }
+        });
+
+        JavaPairRDD<String, Integer> wordCounts = stringIntegerJavaPairRDD.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+
+        wordCounts.saveAsTextFile(destination);
+
+        System.out.println(lines.first());
+    }
+}
